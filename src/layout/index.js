ebb7a68bc6423278ac6876ce9ea8138e2cc2b66f
zannien94 <zannie94@gmail.com>
2022-07-04 23:55:33 +0200

    update layout

--- src/layout/index.js
+++ src/layout/index.js
@@ -1,11 +1,25 @@
 import * as React from 'react'
+import styled from '@emotion/styled'
+import { useLocation } from 'react-router-dom'
+import { Box } from '@mui/material'
 import NavBar from '../components/NavBar'
 
+const Main = styled.main`
+  padding: 20px;
+`
+
 function Layout({ children }) {
+  const location = useLocation()
+
+  const pathName = location.pathname.replace('/', ' ').toUpperCase()
+
   return (
     <>
       <NavBar />
-      <main>{children}</main>
+      <Box margin='16px'>
+        <h2> {pathName}</h2>
+      </Box>
+      <Main>{children}</Main>
     </>
   )
 }
