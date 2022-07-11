f24dd04a6152bf62a703b918d0858d18f78a8439
zannien94 <zannie94@gmail.com>
2022-07-12 00:38:18 +0200

    clean routes

--- src/Routes.js
+++ src/Routes.js
@@ -1,11 +1,8 @@
 import React from 'react'
 import { Routes as Router, Route } from 'react-router-dom'
 import Layout from './layout'
-import Home from './pages/Home'
 import About from './pages/About'
 import Flights from './pages/Flights'
-import Booking from './pages/Booking'
-import Gallery from './pages/Gallery'
 import Contact from './pages/Contact'
 import FlightDetails from './pages/FlightDetails'
 
@@ -13,11 +10,8 @@ function Routes() {
   return (
     <Layout>
       <Router>
-        <Route path='/' element={<Home />} />
+        <Route path='/' element={<Flights />} />
         <Route path='about' element={<About />} />
-        <Route path='flights' element={<Flights />} />
-        <Route path='booking' element={<Booking />} />
-        <Route path='gallery' element={<Gallery />} />
         <Route path='contact' element={<Contact />} />
         <Route path='flights/:id' element={<FlightDetails />} />
       </Router>
