2965b4fc9e97d50b46c3c3edf6e123d7a24dca84
zannien94 <zannie94@gmail.com>
2022-07-14 23:30:59 +0200

    Updates

---
+++ precache-manifest.a769eaafda9c30ccd9e1dd2d97f3febf.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "a9e1d80de6076b7fa833",
+    "url": "/flight-app-frontend/static/css/main.e75e3f77.chunk.css"
+  },
+  {
+    "revision": "a9e1d80de6076b7fa833",
+    "url": "/flight-app-frontend/static/js/main.a9e1d80d.chunk.js"
+  },
+  {
+    "revision": "eca5b609b0ef895077f8",
+    "url": "/flight-app-frontend/static/js/1.eca5b609.chunk.js"
+  },
+  {
+    "revision": "1d0f7b37024bd9c5162d",
+    "url": "/flight-app-frontend/static/js/runtime~main.1d0f7b37.js"
+  },
+  {
+    "revision": "0a1eb11aa152d028252682c9e58bc9e4",
+    "url": "/flight-app-frontend/index.html"
+  }
+];
