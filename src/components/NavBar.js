f59e3ba7eeb6ff3d48b8c83ddd378d1df78c9659
zannien94 <zannie94@gmail.com>
2022-07-14 17:39:51 +0200

    update nav bar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,5 +1,4 @@
 import * as React from 'react'
-import Cookies from 'js-cookie'
 import { NavLink, useNavigate } from 'react-router-dom'
 import styled from '@emotion/styled'
 import AppBar from '@mui/material/AppBar'
@@ -7,8 +6,8 @@ import Box from '@mui/material/Box'
 import Toolbar from '@mui/material/Toolbar'
 import Button from '@mui/material/Button'
 import IconButton from '@mui/material/IconButton'
-import MenuIcon from '@mui/icons-material/Menu'
-import { useGlobalState, setGlobalState } from '../globalState'
+import UserMenu from './UserMenu'
+import { useGlobalState } from '../globalState'
 
 const LinkWrapper = styled.div`
   flex-grow: 1;
@@ -45,11 +44,6 @@ export default function NavBar() {
     navigate(`../login`)
   }
 
-  const handleLogout = () => {
-    setGlobalState('isUserLogged', false)
-    Cookies.remove('isUserLogged')
-  }
-
   return (
     <Box sx={{ flexGrow: 1 }}>
       <AppBar position='fixed' sx={{ background: 'skyblue' }}>
@@ -60,19 +54,18 @@ export default function NavBar() {
             color='inherit'
             aria-label='menu'
             sx={{ mr: 2 }}
-          >
-            <MenuIcon />
-          </IconButton>
+          ></IconButton>
           <Wrapper to='/' text='Flights' />
           <Wrapper to='/about' text='About us' />
           <Wrapper to='/contact' text='Contact us' />
           <Button
-            onClick={isUserLogged ? handleLogout : handleRedirect}
+            onClick={!isUserLogged && handleRedirect}
             color='inherit'
             sx={{ ml: 'auto' }}
           >
-            {isUserLogged ? 'Logout' : 'Login'}
+            {!isUserLogged && 'Login'}
           </Button>
+          {isUserLogged && <UserMenu />}
         </Toolbar>
       </AppBar>
     </Box>
